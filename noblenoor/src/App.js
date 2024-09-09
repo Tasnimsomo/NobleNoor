c8d84078c4bcd0359f3186e5b96e390e507c6358
Nahima-ayni <nahimamadey1@gmail.com>
2024-09-09 08:36:36 +0300

    adding routes in the app components

--- noblenoor/src/App.js
+++ noblenoor/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Header/Header.js';
+import NavMenu from './Menu/NavMenu.js';
+import Login from './Profile/Login.js';
+import Signup from './Profile/Signup.js';
+import AccountPage from './Profile/AccountPage.js';
+import HeroSection from './Hero/HeroSection.js';
+import Collection from './Collection/Collection.js';
+import Cart from './Cart/Cart.js';
+import Checkout from './Cart/Checkout.js';
+import Footer from './Footer/footer.js';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (userData) => {
+    setIsAuthenticated(true);
+    setUser(userData);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    setUser(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header isAuthenticated={isAuthenticated} />
+        <NavMenu />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HeroSection />
+              <Collection titles={["Everyday Abayas", "Summer Collection", "Professional Abayas", "Occasion Abayas", "Jewelry"]} />
+            </>
+          } />
+          <Route path="/collection/:collectionName" element={<Collection isFullView={true} />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} /> {/* Add this new route */}
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route path="/signup" element={<Signup onSignup={handleLogin} />} />
+          <Route path="/account" element={
+            isAuthenticated ? <AccountPage user={user} onLogout={handleLogout} /> : <Login onLogin={handleLogin} />
+          } />
+        </Routes>
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
