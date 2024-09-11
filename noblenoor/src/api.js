8dbde318fe6525eb714575d8a33791cde1249eaf
Nahima-ayni <nahimamadey1@gmail.com>
2024-09-11 07:35:31 +0300

    Profile routing

--- noblenoor/src/api.js
+++ noblenoor/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'http://localhost:5000/';
 
 // Register a new user
 export const register = async (userData) => {
