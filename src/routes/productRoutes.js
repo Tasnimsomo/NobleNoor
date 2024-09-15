c286e81fe9b3bcd482e1c852dd84adc8a2dfbc38
Tasnimsomo <tasnasomo@gmail.com>
2024-09-15 09:28:33 +0400

    get all products

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,14 +1,8 @@
-// export the express module
 const express = require('express');
-
-// create an instance of an express router
 const router = express.Router();
+const productController = require('../controllers/productController'); // Ensure this path is correct
 
-// import the productController module which contains functions to handle product-related operations
-const productController = require('../controllers/productController');
+// Route to get all products
+router.get('/', productController.getAllProducts); // This will handle GET requests to /products
 
-// define a route that handles GET requests to the root path to get all products
-router.get('/search', productController.getAllProducts); // Corrected function name
-
-// export router instance for use in other parts of the application
 module.exports = router;
