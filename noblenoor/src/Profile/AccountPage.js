e5a7a6e1d1aeeec3620ff03c5d8fbcc9a2fd4505
Nahima-ayni <nahimamadey1@gmail.com>
2024-09-20 13:08:00 +0300

    accounts page login and logout

--- noblenoor/src/Profile/AccountPage.js
+++ noblenoor/src/Profile/AccountPage.js
@@ -4,19 +4,8 @@ import './AccountPage.css';
 const AccountPage = () => {
   return (
     <div className="account-page">
-      <h1>Account</h1>
-      <a href="#" className="log-out">Log out</a>
-      
-      <div className="account-content">
-        <div className="order-history">
-          <h2>Order history</h2>
-          <p>You haven't placed any orders yet.</p>
-        </div>
-        
-        <div className="account-details">
-          <h2>Account details</h2>
-        </div>
-      </div>
+      <h1>Welcome to account's page</h1>
+      <a href="/login" className="log-out">Log out</a>
     </div>
   );
 };
